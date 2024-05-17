832727ead7b6fec0decd17402c942e3963e13a87
ddolly518 <20211109@sungshin.ac.kr>
2024-05-17 11:33:25 +0900

    build : 개발 환경 세팅

---
+++ .github/main.js
@@ -0,0 +1,7 @@
+const port = 80, // 포트 번호 지정
+    http = require("http"),
+    httpStatus = require("http-status-codes");
+
+// 애플리케이션 서버가 지정한 포트를 수신하도록 설정
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
