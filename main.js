278adce25e8f94192ede423c71dbcd94860b8154
jiyuuuun <intesys9802@gmail.com>
2024-05-20 15:39:34 +0000

    HomePage-re

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const port = 80, // 포트 번호 지정
-    http = require("http"),
-    httpStatus = require("http-status-codes"),
-    app = http.createServer();
-
-
-// 애플리케이션 서버가 지정한 포트를 수신하도록 설정
-app.listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
-=======
 "use strict";
 
 const fs = require("fs"),
@@ -34,4 +23,3 @@ const fs = require("fs"),
 
 app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
->>>>>>> homepage
