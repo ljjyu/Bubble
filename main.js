2576e1844006904d5326ec74cc9fabaaec22ee39
ddolly518 <20211109@sungshin.ac.kr>
2024-05-27 22:40:24 +0900

    build : Controller.js

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 "use strict";
 
-const fs = require("fs"),
-  port = 80,
-  http = require("http"),
-  httpStatus = require("http-status-codes"),
-  router = require("./router"),
-  plainTextContentType = {
-    "Content-Type":"text/plain"
-  },
-  htmlContentType = {
-    "Content-Type":"text/html"
-  },
-  customReadFile = (file, res) => {
-    fs.readFile(`./${file}`, (errors, data) => {
-          if (errors) {
-            console.log("Error reading the file...");
-          }
-          res.end(data);
-    });
-  };
-router.get("/", (req, res) => {
-    res.writeHead(httpStatus.OK, htmlContentType);
-    customReadFile("views/index.html", res);
+const express = require("express"),
+    app = express(),
+    homeController = require("./controllers/homeController"),
+    errorController = require("./controllers/errorController"),
+    layouts = require("express-ejs-layouts");
+
+app.set("port", process.env.PORT || 80);
+app.set("view engine", "ejs"); // 애플리케이션 뷰 엔진을 ejs로 설정
+// 레이아웃 설정
+//app.use(layouts);
+// 데이터 파싱
+app.use(
+    express.urlencoded({
+        extended:false
+    })
+);
+app.use(express.json());
+// 정적 뷰 제공
+app.use(express.static("public"));
+
+// 라우트 등록
+app.get("/", homeController.showIndex);
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+app.listen(app.get("port"), () => {
+    console.log(`Server running on port: ${app.get("port")}`);
 });
-http.createServer(router.handle).listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
