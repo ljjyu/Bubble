68f60f39a5a24da1248adf84eae1a3edfa446a7c
obongsik <zkfkdzhdo@naver.com>
2024-05-29 17:16:55 +0900

    html연결

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const fs = require("fs"),
   };
 router.get("/", (req, res) => {
     res.writeHead(httpStatus.OK, htmlContentType);
-    customReadFile("views/index.html", res);
+    customReadFile("views/userHome.html", res);
 });
 http.createServer(router.handle).listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
