c625bbfc96c59b5cf05455f23454858b00098a2f
xoyulkim <pejkjh02@naver.com>
2024-05-30 16:10:04 +0900

    user

--- main.js
+++ main.js
@@ -27,6 +27,5 @@ router.get("/userMain", (req, res) => {
   res.writeHead(httpStatus.OK, htmlContentType);
   customReadFile("views/userMain.html", res);
 });
-
 http.createServer(router.handle).listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
