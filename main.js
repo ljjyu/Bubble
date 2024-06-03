c88542e80edecc98cdd70e444114b24c1d316776
obongsik <zkfkdzhdo@naver.com>
2024-06-03 14:16:54 +0900

    main

--- main.js
+++ main.js
@@ -5,18 +5,50 @@ const express = require("express"),
     path = require('path'),
     homeController = require("./controllers/homeController"),
     errorController = require("./controllers/errorController"),
-    userController = require("./controllers/userController"),
-    //machineController = require("./controllers/machineController"),
-    //reservationController = require("./controllers/reservationController"),
+    subscriberController = require("./controllers/subscriberController"),
+    machineController = require("./controllers/machineController"),
+    reservationController = require("./controllers/reservationController"),
     layouts = require("express-ejs-layouts"),
     db = require("./models/index"),
     Sequelize = db.Sequelize,
     Op = Sequelize.Op;
 
-/*db.sequelize.sync(); // 모델 동기화
+db.sequelize.sync(); // 모델 동기화
 const Subscriber = db.subscriber;
 const Machine = db.machine;
-const Reservation = db.reservation;*/
+const Reservation = db.reservation;
+/*let test_create = async () => {
+    try {
+        await Machine.bulkCreate([
+            {
+                name: "Jon",
+                state: "ok"
+            }, {
+                name: "Min",
+                state: "no"
+            }
+        ]);
+    } catch (err) {
+        console.error("Error creating machines:", err);
+    }
+};
+// SELECT
+let test_find = async () => {
+    try {
+        let myQuery = await Machine.findAll({
+            where: { name: "Jon", state: {[Op.like]: "%no%"}}
+        });
+        console.log(myQuery);
+    } catch (err) {
+        console.error("Error finding machines:", err);
+    }
+}
+let test = async () => {
+    await test_create();
+    await test_find();
+}
+test();*/
+
 app.set("port", process.env.PORT || 80);
 app.set("view engine", "ejs"); // 애플리케이션 뷰 엔진을 ejs로 설정
 app.set('views', path.join(__dirname, 'views'));
@@ -33,9 +65,12 @@ app.use(
 app.use(express.json());
 
 // 라우트 등록
-app.get("/userHome", homeController.showIndex);
-app.get("/userMain", homeController.showIndex1);
-app.get("/userReserve", homeController.showIndex2);
+app.get("/subscribers/getSubscriber", subscriberController.getAllSubscribers);
+app.get("/subscribers/subscriber", subscriberController.getSubscriptionPage); // 폼 입력이 가능한 웹 페이지 렌더링
+app.post("/subscribers/subscribe", subscriberController.saveSubscriber); // 넘겨받은 POST 데이터 저장 및 처리
+app.get("/getMachine",machineController.getAllMachines);
+app.get("/getReservation",reservationController.getAllReservations);
+app.get("/", homeController.showIndex);
 
 app.use(errorController.logErrors);
 app.use(errorController.respondNoResourceFound);
@@ -45,21 +80,3 @@ app.use(errorController.internalServerError);
 app.listen(app.get("port"), () => {
     console.log(`Server running on port: ${app.get("port")}`);
 });
-
-
-/*
-router.get("/", (req, res) => {
-    res.writeHead(httpStatus.OK, htmlContentType);
-    customReadFile("views/userHome.html", res);
-});
-router.get("/userMain", (req, res) => {
-  res.writeHead(httpStatus.OK, htmlContentType);
-  customReadFile("views/userMain.html", res);
-});
-router.get("/userReserve", (req, res) => {
-  res.writeHead(httpStatus.OK, htmlContentType);
-  customReadFile("views/userReserve.html", res);
-});
->>>>>>> 8b9cb3bed2ac370d6e45393ac39de0ca9959ddca*/
-
-
