f5d074c333cfd895aae3ea9b42ed1d56eb95c036
ddolly518 <20211109@sungshin.ac.kr>
2024-06-03 16:52:54 +0900

    feat : login test

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,11 +12,11 @@ exports.login = async (req, res) => {
         // 사용자가 존재하고 비밀번호가 일치하는지 확인
         if (user && await bcrypt.compare(password, user.password)) {
             // 인증 성공 시 세션에 사용자 정보 저장
-            req.session.user = {
+            /*req.session.user = {
                 id: user.id,
                 email: user.email
                 // 필요한 사용자 정보 추가
-            };
+            };*/
             res.redirect('/userMain'); // 사용자 메인 페이지로 리다이렉트
         } else {
             res.send('Invalid email or password');
