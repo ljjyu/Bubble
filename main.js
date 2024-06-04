a6f9561e39dcaab4b7485d7c2ca919c4b7808e73
jiyuuuun <intesys9802@gmail.com>
2024-06-04 19:25:22 +0000

    날씨 기능 완료

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const express = require("express"),
     subscriberController = require("./controllers/subscriberController"),
     machineController = require("./controllers/machineController"),
     reservationController = require("./controllers/reservationController"),
+    weatherController = require("./controllers/weatherController"),
     layouts = require("express-ejs-layouts"),
     db = require("./models/index"),
     Sequelize = db.Sequelize,
@@ -17,46 +18,15 @@ db.sequelize.sync(); // 모델 동기화
 const Subscriber = db.subscriber;
 const Machine = db.machine;
 const Reservation = db.reservation;
-/*let test_create = async () => {
-    try {
-        await Machine.bulkCreate([
-            {
-                name: "Jon",
-                state: "ok"
-            }, {
-                name: "Min",
-                state: "no"
-            }
-        ]);
-    } catch (err) {
-        console.error("Error creating machines:", err);
-    }
-};
-// SELECT
-let test_find = async () => {
-    try {
-        let myQuery = await Machine.findAll({
-            where: { name: "Jon", state: {[Op.like]: "%no%"}}
-        });
-        console.log(myQuery);
-    } catch (err) {
-        console.error("Error finding machines:", err);
-    }
-}
-let test = async () => {
-    await test_create();
-    await test_find();
-}
-test();*/
 
 app.set("port", process.env.PORT || 80);
 app.set("view engine", "ejs"); // 애플리케이션 뷰 엔진을 ejs로 설정
 app.set('views', path.join(__dirname, 'views'));
+
 // 정적 뷰 제공
 app.use(express.static("public"));
 // 레이아웃 설정
-//app.use(layouts);
-// 데이터 파싱
+app.use(layouts);// 데이터 파싱
 app.use(
     express.urlencoded({
         extended:false
@@ -70,6 +40,7 @@ app.get("/subscribers/subscriber", subscriberController.getSubscriptionPage); //
 app.post("/subscribers/subscribe", subscriberController.saveSubscriber); // 넘겨받은 POST 데이터 저장 및 처리
 app.get("/getMachine",machineController.getAllMachines);
 app.get("/getReservation",reservationController.getAllReservations);
+app.use("/getWeather", weatherController);
 app.get("/", homeController.showIndex);
 
 app.use(errorController.logErrors);
@@ -77,6 +48,8 @@ app.use(errorController.respondNoResourceFound);
 app.use(errorController.respondInternalError);
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
+
+// 서버 리스닝
 app.listen(app.get("port"), () => {
     console.log(`Server running on port: ${app.get("port")}`);
 });
