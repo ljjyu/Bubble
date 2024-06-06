3bdc1c3c7278b4e47baf915a175107e585c0936c
xoyulkim <pejkjh02@naver.com>
2024-06-06 17:47:04 +0900

    수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 "use strict";
 
 exports.showIndex = (req, res) => {
-    res.render("/user/userHome");
+    res.render("user/userHome");
 };
 exports.showIndex1 = (req, res) => {
-    res.render("/user/userMain");
+    res.render("user/userMain");
 };
 exports.showIndex2 = (req, res) => {
-    res.render("/user/userReserve");
+    res.render("user/userReserve");
 };
