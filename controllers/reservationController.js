0b3a3433b278e81c07f9115438360e8b0762dd4a
obongsik <zkfkdzhdo@naver.com>
2024-06-06 14:46:07 +0900

    db연결시도1

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -6,7 +6,7 @@ exports.getAllReservations = async (req, res) => {
     try {
         const data = await Reservation.findAll();
         console.log(data);
-        res.render("getReservation", {reservations: data});
+        res.render("userHome", { reservations: data });
     } catch (err) {
         res.status(500).send({
             message: err.message
@@ -16,17 +16,17 @@ exports.getAllReservations = async (req, res) => {
 
 exports.createReservation = async (req, res) => {
     try {
-        const { usageTime, reservationDate, Location } = req.body;
+        const { machineType, machineNumber, reservationTime } = req.body;
         const reservationNumber = uuidv4(); // Generate a unique reservation number
 
         const newReservation = await Reservation.create({
             reservationNumber,
-            usageTime,
-            reservationDate,
-            Location
+            usageTime: reservationTime, // 예약 시간을 문자열로 저장
+            reservationDate: new Date(reservationTime), // 예약 날짜를 Date 객체로 저장
+            Location: `${machineType} ${machineNumber}`
         });
 
-        res.status(201).send(newReservation);
+        res.redirect("/getReservation");
     } catch (err) {
         res.status(500).send({
             message: err.message
