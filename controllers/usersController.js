43fc9eb0738efbe6c9f94982de78e025b3831c9f
ddolly518 <20211109@sungshin.ac.kr>
2024-06-06 14:45:38 +0900

    fix : login test

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,23 +3,20 @@ const db = require("../models/index"),
     Op = db.Sequelize.Op,
     bcrypt = require('bcrypt');
 
-exports.login = async (req, res) => {
+exports.authenticate = async (req, res) => {
     const { email, password } = req.body;
     try {
         // 이메일로 사용자 조회
-        const user = await Subscriber.findOne({ where: { email } });
+        const user = await Subscriber.findOne({ where: { email:req.body.email } });
 
         // 사용자가 존재하고 비밀번호가 일치하는지 확인
-        if (user && await bcrypt.compare(password, user.password)) {
-            // 인증 성공 시 세션에 사용자 정보 저장
-            /*req.session.user = {
-                id: user.id,
-                email: user.email
-                // 필요한 사용자 정보 추가
-            };*/
-            res.redirect('/userMain'); // 사용자 메인 페이지로 리다이렉트
+        if ((user && user.password === req.body.password) {
+            req.flash("logged in successfully!");
+            res.locals.redirect('/userMain'); // 사용자 메인 페이지로 리다이렉트
+            res.locals.user = user;
         } else {
-            res.send('Invalid email or password');
+            req.flash("error", "Your account or password is incorrect. Please try again or contact your system administrator!");
+            res.locals.redirect('/');
         }
     } catch (error) {
         console.error('Error logging in:', error);
--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const express = require("express"),
     bodyParser = require('body-parser'),
     session = require('express-session'),
     flash = require('connect-flash'),
+    cookieParser = require("cookie-parser"),
     passport = require("passport"),
     bcrypt = require('bcrypt'),
     db = require("./models/index"),
@@ -19,37 +20,6 @@ const express = require("express"),
 
 db.sequelize.sync(); // 모델 동기화
 const Subscriber = db.subscriber;
-/*let test_create = async () => {
-    try {
-        await Subscriber.bulkCreate([
-            {
-                name: "Jon",
-                email: "jon@naver.com"
-            }, {
-                name: "Min",
-                email: "min@gamil.com"
-            }
-        ]);
-    } catch (err) {
-        console.error("Error creating subscribers:", err);
-    }
-};
-// SELECT
-let test_find = async () => {
-    try {
-        let myQuery = await Subscriber.findAll({
-            where: { name: "Jon", email: {[Op.like]: "%naver%"}}
-        });
-        console.log(myQuery);
-    } catch (err) {
-        console.error("Error finding subscribers:", err);
-    }
-}
-let test = async () => {
-    await test_create();
-    await test_find();
-}
-test();*/
 
 app.set("port", process.env.PORT || 80);
 app.set("view engine", "ejs"); // 애플리케이션 뷰 엔진을 ejs로 설정
@@ -71,7 +41,7 @@ app.get("/subscribers/getSubscriber", subscriberController.getAllSubscribers);
 app.get("/subscribers/subscriber", subscriberController.getSubscriptionPage); // 폼 입력이 가능한 웹 페이지 렌더링
 app.post("/subscribers/subscribe", subscriberController.saveSubscriber); // 넘겨받은 POST 데이터 저장 및 처리
 app.get("/", homeController.showIndex);
-app.post("/", usersController.login);
+app.post("/", usersController.authenticate);
 app.get("/userMain", homeController.showIndex2);
 
 app.use(errorController.logErrors);
