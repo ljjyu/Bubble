c3b785eeb49cb46cb2de890b1835317c2e863f1b
ddolly518 <20211109@sungshin.ac.kr>
2024-06-06 15:21:55 +0900

    fix : login test

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,11 +12,12 @@ exports.authenticate = async (req, res, next) => {
         if (user && await bcrypt.compare(password, user.password)) {
             req.flash("success","logged in successfully!");
             res.locals.user = user;
-             res.locals.redirect = '/userMain'; // 사용자 메인 페이지로 리다이렉트
+            res.locals.redirect = '/userMain'; // 사용자 메인 페이지로 리다이렉트
         } else {
             req.flash("error", "Your account or password is incorrect. Please try again or contact your system administrator!");
             res.locals.redirect = '/';
         }
+        console.log("Redirecting to:", res.locals.redirect); // 디버깅 로그 추가
         next(); // 다음 미들웨어로 이동
     } catch (error) {
         console.error('Error logging in:', error);
