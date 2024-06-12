9bac48c9a6de020a444546458f601b50d6e2d7c6
ddolly518 <20211109@sungshin.ac.kr>
2024-06-12 14:03:15 +0900

    fix : 예약내역 내림차순 정렬

--- controllers/userHomeController.js
+++ controllers/userHomeController.js
@@ -5,7 +5,7 @@ const db = require("../models/index"),
 exports.getUserReservations = async (req, res) => {
     try {
         // createdAt을 기준으로 내림차순 정렬
-        data = await Reservation.findAll({order: [['createdAt', 'DESC']]});
+        data = await Reservation.findAll({order: [['created_at', 'DESC']]});
         console.log(data);
         res.render("user/userHome", {reservations: data, layout: 'userLayout' });
     } catch (err) {
