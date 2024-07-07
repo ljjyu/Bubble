14569ed4bfd56d1f135a3e3537fdae3bdec5dd3a
ddolly518 <20211109@sungshin.ac.kr>
2024-07-07 15:22:00 +0900

    fix : 예약 지점명

--- controllers/userHomeController.js
+++ controllers/userHomeController.js
@@ -7,10 +7,19 @@ exports.getUserReservations = async (req, res) => {
         // createdAt을 기준으로 내림차순 정렬
         const data = await Reservation.findAll({
             order: [['created_at', 'DESC']],
-            include: {
-                model: db.machine,
-                as: 'machine' // Machine 모델을 include
-            }
+            include: [
+                {
+                    model: Machine,
+                    as: 'machine', // Machine 모델을 include
+                    include: [
+                        {
+                            model: Branch,
+                            as: 'branch', // Branch 모델을 include
+                            attributes: ['branchName'] // 필요한 속성만 선택
+                        }
+                    ]
+                }
+            ]
         });
         res.render("user/userHome", {reservations: data});
     } catch (err) {
