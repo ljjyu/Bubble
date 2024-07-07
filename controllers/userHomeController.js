8da89ffe152bddbc99bf3771f588fe900004d807
ddolly518 <20211109@sungshin.ac.kr>
2024-07-07 15:00:49 +0900

    fix : 예약

--- controllers/userHomeController.js
+++ controllers/userHomeController.js
@@ -5,8 +5,13 @@ const db = require("../models/index"),
 exports.getUserReservations = async (req, res) => {
     try {
         // createdAt을 기준으로 내림차순 정렬
-        data = await Reservation.findAll({order: [['created_at', 'DESC']]});
-        console.log(data);
+        const data = await Reservation.findAll({
+            order: [['created_at', 'DESC']],
+            include: {
+                model: db.machine,
+                as: 'machine' // Machine 모델을 include
+            }
+        });
         res.render("user/userHome", {reservations: data});
     } catch (err) {
         res.status(500).send({
