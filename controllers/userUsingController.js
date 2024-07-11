355981e717763fc73d3c2c894091ec9e00d2e7f6
ddolly518 <20211109@sungshin.ac.kr>
2024-07-11 20:11:59 +0900

    fix : 예약 정보

--- controllers/userUsingController.js
+++ controllers/userUsingController.js
@@ -22,7 +22,7 @@ exports.getUserUsingPage = async (req, res) => {
         });
 
         // 렌더링할 페이지와 데이터
-        res.render('user/userUsing', { reservations: userReservations });
+        res.render('user/userUsing', { reservations: reservations });
     } catch (err) {
         console.error(err);
         res.status(500).send({
