5e0dde0154e13a20f3d1d67fc8c0a846053f7c92
ddolly518 <20211109@sungshin.ac.kr>
2024-07-16 03:37:57 +0900

    fix : index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+
 // 데이터베이스 연결을 위한 sequelize 인스턴스 생성
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 db.sequelize = sequelize;
