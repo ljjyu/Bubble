aeefe53d992f1fdc42c9227407e51c172b196e21
ddolly518 <20211109@sungshin.ac.kr>
2024-07-15 20:24:09 +0900

    fix : 사용자별 예약 내역

--- controllers/userHomeController.js
+++ controllers/userHomeController.js
@@ -9,6 +9,7 @@ exports.getUserReservations = async (req, res) => {
          // 로그인된 사용자의 정보를 가져옵니다.
          const user = req.session.user;
          const subscriberName = user ? user.name : 'Unknown User';
+
         // createdAt을 기준으로 내림차순 정렬
         const data = await Reservation.findAll({
             where: {
