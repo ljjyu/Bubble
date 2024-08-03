6e90dd5a4d1ff8834d918bb05bbb7b5cc37946ec
jiyun <intesys1004@naver.com>
2024-08-03 16:43:51 +0000

    MyPage 다크모드 및 UI

--- public/js/navbarController.js
+++ public/js/navbarController.js
@@ -8,3 +8,29 @@ document.addEventListener('DOMContentLoaded', () => {
         icons.classList.toggle('active');
     });
 });
+
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeButton = document.getElementById("dark-mode");
+    const body = document.body;
+
+    // 다크모드 토글 함수
+    if (darkModeButton) {
+        darkModeButton.addEventListener("click", function() {
+            body.classList.toggle("dark-mode");
+
+            // 현재 상태를 로컬 스토리지에 저장
+            if (body.classList.contains("dark-mode")) {
+                localStorage.setItem("darkMode", "enabled");
+            } else {
+                localStorage.setItem("darkMode", "disabled");
+            }
+        });
+    }
+
+    // 페이지 로드 시 다크모드 상태를 로컬 스토리지에서 읽어옴
+    if (localStorage.getItem("darkMode") === "enabled") {
+        body.classList.add("dark-mode");
+    }
+});
+
