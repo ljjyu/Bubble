4e1b188c9ab01bbe5771fccc42673ca64a717663
ddolly518 <20211109@sungshin.ac.kr>
2024-08-03 16:00:42 +0900

    feat : mypage.ejs 구성 test

--- controllers/myPageController.js
+++ controllers/myPageController.js
@@ -1,7 +1,7 @@
 
 exports.getAllMyPage = async (req, res) => {
     try {
-        res.render("myPage");
+        res.render("showMyPage");
     } catch (err) {
         res.status(500).send({message: err.message});
     }
