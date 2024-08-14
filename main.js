b38505f6db32b51d23d578d9a62baa17d87936a6
ddolly518 <20211109@sungshin.ac.kr>
2024-08-14 13:53:32 +0900

    fix: error 고치기

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const express = require("express"),
     app = express(),
     path = require('path'),
     moment = require('moment-timezone'),
-    moment = require('moment'),
     homeController = require("./controllers/homeController"), // 메인 로그인
     errorController = require("./controllers/errorController"), // 에러 관련
     subscriberController = require("./controllers/subscriberController"), // 회원가입 및 회원 정보
