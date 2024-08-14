cf5c49b291d1db9ee2e7a12dc28fa3f2b6452a69
obongsik <zkfkdzhdo@naver.com>
2024-08-14 11:19:20 +0900

    fix

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -5,8 +5,7 @@ const Subscriber = db.subscriber;
 const TempSubscriber = db.tempSubscriber;
 const Branch = db.branch;
 const Machine = db.machine;
-const TempSubscriber = require('./models/TempSubscriber'); // TempSubscriber 모델
-const Subscriber = require('./models/Subscriber'); // Subscriber 모델
+
 
 exports.getAllSubscribers = async (req, res) => {
     try {
