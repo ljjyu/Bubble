6cf2b7e952085bcbaada3663e3e498ef9eea92b6
KMS <80440158+ddolly518@users.noreply.github.com>
2024-11-27 23:48:57 +0900

    Update www.js

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,9 +1,8 @@
-"use strice"
+"use strict"
 
-const app = require("../main"):
-
+const app = require("../main");
 const PORT = process.env.PORT || 80;
 
-app.listen(PORT, () => {
-	console.log(`서버가동,포트번호:${PORT}`)
+app.listen(PORT, () =>{
+    console.log(`서버가동,포트번호:${PORT}`)
 });
