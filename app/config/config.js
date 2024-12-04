c04098db433265f900401939bb707cea95dc1ba7
ddolly518 <20211109@sungshin.ac.kr>
2024-12-04 09:29:20 +0000

    Update main.js

--- app/config/config.js
+++ app/config/config.js
@@ -6,14 +6,14 @@ const dbConfig = {
         password: process.env.DB_PW,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: "mysql",
+        dialect: process.env.DB_DIALECT || "mysql",
     },
     production: {
         username: process.env.DB_USER,
         password: process.env.DB_PW,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: "mysql",
+        dialect: process.env.DB_DIALECT || "mysql",
         logging: false,
     }
 };
